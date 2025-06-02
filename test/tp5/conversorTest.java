d21b0fbb605f747b619892bb0f8f301fb117266e
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-02 17:38:45 -0300

    Nuevos before y after en conversorTest
    Se agregaron beforeAll, afterAll,beforeEach y afterEach
    
    Co-Authored-By: AlexFrame <178439170+AlecFrame@users.noreply.github.com>
    Co-Authored-By: Ariel Ismael Miranda Salmin <178439453+Ismael7058@users.noreply.github.com>
    Co-Authored-By: Ignacio989 <163351249+FALLYNG@users.noreply.github.com>
    Co-Authored-By: Bongo117 <178439181+Bongo117@users.noreply.github.com>

--- test/tp5/conversorTest.java
+++ test/tp5/conversorTest.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
 package tp5;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,35 +16,34 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author juancz55
  */
 public class conversorTest {
-    
-    static Conversor conversor = new Conversor(200); 
-    @Test 
-    public void Suma(){
-    conversor.aumentarSaldo(200);
-        assertEquals(400,conversor.getSaldo() , 0.001);
-    }
-   @Test 
-    public void Resta(){
-     conversor.retirarFisico(200);
-        assertEquals(200,conversor.getSaldo() , 0.001);
-    }         
+
+    static Conversor conversor;
+
     public conversorTest() {
     }
-    
+    //Use las annotation @BeforeClass para inicializar (new) el calculador y mostrar mensaje de bienvenida. Utilice las annotations:
+    //@Before antes de cada método, que muestre un mensaje de la operación que se testea
+    //Use System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() );
+
     @BeforeAll
     public static void setUpClass() {
+        conversor = new Conversor(200);
+        System.out.println("Bienvenido al BeforeAll");
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
+        System.out.println("Prueba finalizada, campos en 0");
     }
-    
+
     @BeforeEach
     public void setUp() {
+        System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName());
     }
-    
+
     @AfterEach
     public void tearDown() {
+        System.out.println("La Operacion ha finalizado");
     }
 
     @Test
@@ -51,5 +51,16 @@ public class conversorTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
+
+    @Test
+    public void Suma() {
+        conversor.aumentarSaldo(200);
+        assertEquals(400, conversor.getSaldo(), 0.001);
+    }
+
+    @Test
+    public void Resta() {
+        conversor.retirarFisico(200);
+        assertEquals(200, conversor.getSaldo(), 0.001);
+    }
 }
